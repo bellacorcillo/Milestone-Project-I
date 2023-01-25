c668934dbfd1188ed627e37f405da91e8a72989f
bellacorcillo <isabellacorcillo@gmail.com>
2023-01-24 19:56:24 -0800

    Adding in words and hints

---
+++ words.js
@@ -0,0 +1,114 @@
+const wordList = [
+    {
+        word: "promenade",
+        hint: "to walk in public"
+    },
+    {
+        word: "facer",
+        hint: "a punch in the face"
+    },
+    {
+        word: "swoon",
+        hint: "to faint in style"
+    },
+    {
+        word: "modiste",
+        hint: "a dressmaker"
+    },
+    {
+        word: "countenance",
+        hint: "a person's facial expression"
+    },
+    {
+        word: "viscount",
+        hint: "a nobility title that is above a baron but below a duke"
+    },
+    {
+        word: "diamond of the first water",
+        hint: "a debutant that has got it going on"
+    },
+    {
+        word: "courtship",
+        hint: "exploring a relationship with intention of marriage"
+    },
+    {
+        word: "dance card",
+        hint: "card on wrist of lady with dance partners"
+    },
+    {
+        word: "debutante",
+        hint: "a young lady making her formal entrance into society"
+    },
+    {
+        word: "dowry",
+        hint: "a payment made by a bride's family to the groom"
+    },
+    {
+        word: "grosvenor square",
+        hint: "a garden square in the Mayfair district of London"
+    },
+    {
+        word: "heir",
+        hint: "the eldest son who will inherit the property and rank of a family"
+    },
+    {
+        word: "love match",
+        hint: "a marriage founded on love"
+    },
+    {
+        word: "mayfair",
+        hint: "an exlusive district in london"
+    },
+    {
+        word: "rake",
+        hint: "a man who avoids romantic commitment"
+    },
+    {
+        word: "regency",
+        hint: "the period in english history between 1811 - 1820"
+    },
+    {
+        word: "rotten row",
+        hint: "a walkway in london known as a meeting spot for the fashionable upper-class"
+    },
+    {
+        word: "spinster",
+        hint: "an unmarried woman beyond the eligible age of marriage"
+    },
+    {
+        word: "sponsor",
+        hint: "an overseer of a young lady's social debut"
+    },
+    {
+        word: "trousseau",
+        hint: "bridal clothing for honeymoon"
+    },
+    {
+        word: "simpleton",
+        hint: "a foolish or stupid person"
+    },
+    {
+        word: "fortnight",
+        hint: "a period of two weeks time"
+    },
+    {
+        word: "drawing room",
+        hint: "a place for a host to entertain guests"
+    },
+    {
+        word: "high in the instep",
+        hint: "a snobby or arrogant person"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "frock",
+        hint: "a dress"
+    },
+    {
+        word: "lady on display",
+        hint: "a woman engaging in the courting season"
+    }
+        
